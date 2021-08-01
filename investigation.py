0ee44db0fe586b4d6ecec85a4645faa4e631d548
Thomas Schlegl <thomas.m.schlegl@gmail.com>
2021-08-01 15:08:56 +0200

    refactored

--- investigation.py
+++ investigation.py
@@ -6,25 +6,68 @@ from greedysearch.pipeline import features_transform
 from shapelettransform.algorithm import ShapeletTransform
 from utils.dump_results import dump_object
 from utils.dump_results import load_object
+from utils.plot_results import create_dataframe
+from utils.plot_results import plot_swarmplot
 
-# %%
+from pyts.datasets import load_gunpoint
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# %% extract shapelets and dump results
+'''
+X_train, X_test, y_train, y_test = load_gunpoint(return_X_y=True)
+
+ST = ShapeletTransform()
+ST.get_top_k_shapelets(X_train=X_train, y_train=y_train, n_shapelets=5, 
+shapelet_min_size=30, shapelet_max_size=40)
+dump_object('01_top5_shapelet_GunPoint_ST', ST)
+
+GSS= GreedyShapeletSearch()
+GSS.get_top_k_shapelets(X_train=X_train, y_train=y_train, scoring_function=fit_svm, 
+n_shapelets=75, shapelet_min_size=25, shapelet_max_size=45)
+dump_object('01_top75_shapelet_GunPoint_GSS', GSS)
+'''
+
+
+
+# %% figure 1
 '''
 visualize mindist of top shapelet for GSS and ST for both training and test data
 goal: demonstrate, that (soft) margin leads to more robust shapelets than IG
 '''
+GSS = load_object('top75_shapelets_GunPoint_GSS') 
+ST = load_object('top5_shapelets_GunPoint_ST')
 
-from pyts.datasets import load_gunpoint
 X_train, X_test, y_train, y_test = load_gunpoint(return_X_y=True)
 
-GSS= GreedyShapeletSearch()
-GSS.get_top_k_shapelets(X_train=X_train, y_train=y_train, scoring_function=fit_svm, 
-n_shapelets=1, shapelet_min_size=30, shapelet_max_size=40)
-dump_object('01_top_shapelet_GunPoint_GSS', GSS)
+df_gss_train = create_dataframe(features_transform(X_train, GSS)[:,0], y_train, 'train', 'GSS')
+df_gss_test = create_dataframe(features_transform(X_test, GSS)[:,0], y_test, 'train', 'GSS')
+df_gss = pd.concat([df_gss_train, df_gss_test])
+plot_swarmplot(df_gss, 'swarmplot_gss_top_shapelet')
 
-ST = ShapeletTransform()
-ST.get_top_k_shapelets(X_train=X_train, y_train=y_train, n_shapelets=1, 
-shapelet_min_size=30, shapelet_max_size=40)
-dump_object('01_top_shapelet_GunPoint_ST', ST)
+df_st_train = create_dataframe(features_transform(X_train, ST)[:,0], y_train, 'train', 'ST')
+df_st_test = create_dataframe(features_transform(X_test, ST)[:,0], y_test, 'train', 'ST')
+df_st = pd.concat([df_gss_train, df_gss_test])
+plot_swarmplot(df_st, 'swarmplot_st_top_shapelet')
+
+
+
+
+# %%
+
+GSS = load_object('top75_shapelets_GunPoint_GSS') 
+ST = load_object('top5_shapelets_GunPoint_ST')
+
+# %%
+listi = []
+for i in ST.shapelets:
+    listi.append(i)
+
+
+
+
 
 # %%
 '''
--- utils/plot_results.py
+++ utils/plot_results.py
@@ -1,12 +1,50 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 28 13:32:43 2021
 
-
-"""
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 from sklearn.preprocessing import StandardScaler  
+
+
+def create_dataframe(features, labels, dataset, method):
+    df = pd.DataFrame(features)
+    df.astype('float32').dtypes
+    df.columns = [f'Feature {i}' for i in np.arange(df.shape[1])]
+    df['Class label'] = labels
+    df['Dataset'] = dataset
+    df['method'] = 'GSS'
+    return df
+
+def plot_swarmplot(df, filename):
+    fig = sns.swarmplot(x="Dataset", y="Feature 0", hue="Class label", data=df)
+    plt.savefig(f'results/{filename}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def plot_shapelets(obj):
 
